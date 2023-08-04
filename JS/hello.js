17de8d4ecf98aa1c401b2b5ba7deefb1ae18d4cc
Leiou1939 <74086412+Leiou1939@users.noreply.github.com>
2023-08-04 16:14:27 +0800

    Add files via upload

---
+++ JS/hello.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+var typed = new Typed('.type',{
+    strings:['欢迎','Welcome','Добро пожаловать.','Bienvenue','Bonvenon','Velkommen','benvenuto','Vítejte','Καλώς ήρθατε','ようこそ','ยินดีต้อนรับ','Hoşgeldiniz.','advenisti','witamy','Maligayang pagdating','Selamat Datang'],
+    typeSpeed:70,
+    loop:true,
+    backSpeed:30,
+})
